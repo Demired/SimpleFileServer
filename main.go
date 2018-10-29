03098d60284c7c4e707f6edccc760fe97a3afca1
zhangyuan <nevermakeyoucrytoli@gmail.com>
2018-10-29 18:51:01 +0800

    v2

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
+	"net"
 	"net/http"
 	"os"
 	"strconv"
@@ -51,9 +53,56 @@ func main() {
 			dir = "."
 		}
 	}
+
+	var wanIPURL = "https://myip.ipip.net/s"
+
+	wanIP := httpGet(wanIPURL)
+
+	IntranetIP := getIntranetIP()
+
 	fmt.Printf("listen port is %d\n", port)
+	fmt.Println("----------------------------------------------")
+	fmt.Printf("| loc address : http://127.0.0.1:%d\n", port)
+	fmt.Printf("| lan address : http://%s:%d\n", IntranetIP, port)
+	fmt.Printf("| wan address : http://%s:%d\n", strings.TrimSpace(wanIP), port)
+	fmt.Println("----------------------------------------------")
+
 	err := http.ListenAndServe(fmt.Sprintf(":%d", port), http.FileServer(http.Dir(dir)))
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
+}
+
+func getIntranetIP() string {
+	addrs, err := net.InterfaceAddrs()
+
+	if err != nil {
+		return ""
+	}
+
+	for _, address := range addrs {
+		// 检查ip地址判断是否回环地址
+		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				return ipnet.IP.String()
+			}
+		}
+	}
+
+	return ""
+}
+
+func httpGet(url string) string {
+	resp, err := http.Get(url)
+	if err != nil {
+		// handle error
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		// handle error
+	}
+
+	return string(body)
 }
